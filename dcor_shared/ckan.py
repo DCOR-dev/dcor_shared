58d2bf3d63481eaf489370292b1df81c8d631640
Paul Müller <dev@craban.de>
2020-09-18 07:54:14 +0200

    enh: allow to get the resource path from outside CKAN

--- dcor_shared/ckan.py
+++ dcor_shared/ckan.py
@@ -1,19 +1,39 @@
 import os
+import pathlib
 import warnings
 
 try:
     from ckan.common import config
 except ImportError:
     config = None
-    warnings.warn("CKAN is not installed. Please make sure that the "
-                  + "environment is active. Some functionalities of "
-                  + "dcor_shared are not available!")
 
 
 if config:
     #: CKAN storage path (contains resources, uploaded group, user or
     #: organization images)
     CKAN_STORAGE = config.get('ckan.storage_path', "").rstrip("/")
+else:
+    # Attempt to load the configuration from the actual ckan.ini
+    ckanini = pathlib.Path("/etc/ckan/default/ckan.ini")
+    if ckanini.exists():
+        # parse the ini file
+        with open(ckanini) as fd:
+            for line in fd.readlines():
+                line = line.strip()
+                if line.startswith("#") or line.startswith("["):
+                    continue
+                elif line.count("="):
+                    key, value = line.split("=", 1)
+                    if key == "ckan.storage_path":
+                        CKAN_STORAGE = value.strip()
+                        break
+            else:
+                CKAN_STORAGE = None
+    if CKAN_STORAGE is None:
+        warnings.warn("CKAN is not installed. Please make sure that the "
+                      + "environment is active or the ckan.ini file is in "
+                      + "its usual place. Some functionalities of "
+                      + "dcor_shared are not available!")
 
 
 def get_resource_path(rid, create_dirs=False):
