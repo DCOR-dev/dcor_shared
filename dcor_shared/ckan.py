78d74e8ec2460d99ad7095d90d5e1dd11488980e
Paul Müller <dev@craban.de>
2022-01-08 01:32:02 +0100

    feat: add paths submodule with common DCOR paths

--- dcor_shared/ckan.py
+++ dcor_shared/ckan.py
@@ -1,19 +1,56 @@
 import os
 import pathlib
-import warnings
 
 try:
-    from ckan.common import config
+    from ckan.common import config as ckan_config
 except ImportError:
-    config = None
+    ckan_config = None
+
+from .parse import get_ini_config_option
+
+
+def get_ckan_config_option(option):
+    """Return the CKAN configuration option
+
+    If `ckan.common.config` is imported, then the configuration is
+    taken from there. Else, the configuration is taken from the
+    configuration file returned by :func:`get_ckan_config_path`.
+    """
+    if ckan_config:
+        # Get from current configuration
+        opt = ckan_config.get(option, "")
+    else:
+        opt = get_ini_config_option(option,
+                                    get_ckan_config_path())
+    return opt
+
+
+def get_ckan_config_path():
+    """Return path to ckan.ini (prefer from environment)"""
+    default = "/etc/ckan/default/ckan.ini"
+    return pathlib.Path(os.environ.get("CKAN_INI", default))
+
+
+def get_ckan_storage_path():
+    """Return ckan.storage_path
+
+    contains resources, uploaded group, user or organization images
+    """
+    return pathlib.Path(get_ckan_config_option("ckan.storage_path"))
+
+
+def get_ckan_webassets_path():
+    """Return path to CKAN webassets"""
+    return pathlib.Path(get_ckan_config_option("ckan.webassets.path"))
 
 
 def get_resource_path(rid, create_dirs=False):
-    resources_path = get_storage_path() + "/resources"
-    pdir = f"{resources_path}/{rid[:3]}/{rid[3:6]}"
-    path = f"{pdir}/{rid[6:]}"
+    resources_path = get_ckan_storage_path() / "resources"
+    pdir = resources_path / rid[:3] / rid[3:6]
+    path = pdir / rid[6:]
     if create_dirs:
         try:
+            pdir.mkdir(parents=True, exist_ok=True)
             os.makedirs(pdir)
             os.chown(pdir,
                      os.stat(resources_path).st_uid,
@@ -21,37 +58,3 @@ def get_resource_path(rid, create_dirs=False):
         except OSError:
             pass
     return pathlib.Path(path)
-
-
-def get_storage_path():
-    if config:
-        #: CKAN storage path (contains resources, uploaded group, user or
-        #: organization images)
-        cks = config.get('ckan.storage_path', "").rstrip("/")
-    else:
-        # Attempt to load the configuration from the actual ckan.ini
-        cks = None
-        ckanini = pathlib.Path("/etc/ckan/default/ckan.ini")
-        if ckanini.exists():
-            # parse the ini file
-            with open(ckanini) as fd:
-                for line in fd.readlines():
-                    line = line.strip()
-                    if line.startswith("#") or line.startswith("["):
-                        continue
-                    elif line.count("="):
-                        key, value = line.split("=", 1)
-                        if key == "ckan.storage_path":
-                            cks = value.strip()
-                            break
-        if cks is None:
-            warnings.warn("CKAN is not installed. Please make sure that the "
-                          + "environment is active or the ckan.ini file is in "
-                          + "its usual place. Some functionalities of "
-                          + "dcor_shared are not available!")
-    return cks
-
-
-#: CKAN storage path at initialization. If the path changes (e.g. due to
-#: tests, please use :func:`get_storage_path`.
-CKAN_STORAGE = get_storage_path()
